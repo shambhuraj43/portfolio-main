2b507485834d8f8af33ac6373dce160317b8b131
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-12-06 19:11:29 -0500

    renamed STATIC_DIR variable

--- portfolio_website/portfolio_website/settings.py
+++ portfolio_website/portfolio_website/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIRS = [STATIC_DIR,]
+STATICFILES_DIRS = [STATIC_DIR,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
