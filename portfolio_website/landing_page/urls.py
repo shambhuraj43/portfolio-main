0c3dae274ea7a65c9e999987a0a2dd1784ed1478
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-12-06 19:10:42 -0500

    updated urls.py with new templates

--- portfolio_website/landing_page/urls.py
+++ portfolio_website/landing_page/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from landing_page import views
 
+app_name = 'landing_page'
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name='index'),
+    path('generic/', views.generic, name='generic'),
+    path('generic/', views.generic, name='generic'),
+    path('elements/', views.elements, name='elements'),
 ]
