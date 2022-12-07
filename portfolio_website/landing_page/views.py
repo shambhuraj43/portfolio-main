e6221a6c3e18101d3d7739ae820c400768f23d6b
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2022-12-06 19:10:58 -0500

    added views for new templates

--- portfolio_website/landing_page/views.py
+++ portfolio_website/landing_page/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "landing_page/index.html")
+
+def generic(request):
+    return render(request, 'landing_page/generic.html')
+
+def elements(request):
+    return render(request, 'landing_page/elements.html')
     
