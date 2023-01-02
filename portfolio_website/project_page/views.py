48b1a3bd6d9ec242e46151b7299a197f069b3523
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-02 02:22:34 -0500

    changed test view

--- portfolio_website/project_page/views.py
+++ portfolio_website/project_page/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "project_page/index.html")
+    return render(request, "project_page/base_project_page.html")
 
