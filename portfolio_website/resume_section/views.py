bff9be899ab622ce54b4b588e488ad05d11918a5
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-03 02:50:34 -0500

    updated route to base.html

--- portfolio_website/resume_section/views.py
+++ portfolio_website/resume_section/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def resume_page(request):
-    return render(request, "resume_section/index.html")
+    return render(request, "resume_section/base.html")
