9bed4686d9d61f8d5408af8c5d3b7fe307783df3
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-02 22:29:39 -0500

    modified context for view

--- portfolio_website/project_page/views.py
+++ portfolio_website/project_page/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request, project_id):
-    s = f"<h1>Project Name:{project_id}<h1>"
-    # return HttpResponse(s)
+
     context = {
-        "project_name":"PROJECT_NAME",
-        "project_description":"PROJECT_DESCRIPTION",
-        "technolgies_used":[
-            {"name":"TECHNOLGY_NAME"},
-            {"description":"TECHNOLOGY DESCRIPTION"},
+        "project_name": "PROJECT_NAME",
+        "project_description": "PROJECT_DESCRIPTION",
+        "technolgies_used": [
+            {
+                "name": "docker",
+                "description": "TECHNOLOGY DESCRIPTION",
+                "icon":'docker'
+            },
+            {
+                "name": "docker",
+                "description": "TECHNOLOGY DESCRIPTION",
+                "icon":"none"
+            },
+            {
+                "name": "docker",
+                "description": "TECHNOLOGY DESCRIPTION",
+                "icon":"none"
+            },
         ],
-        "installation_requirements":"INSTALLATION REQUIREMENTS",
-        "github_repo_link":"GITHUB REPO LINK"
+        "installation_requirements": "INSTALLATION REQUIREMENTS",
+        "requirements_file":"Requirements.txt",
+        "requirements_file_link":"https://github.com/shambhuraj43/portfolio-main",
+        "github_repo_link": "https://github.com/shambhuraj43/portfolio-main"
     }
     return render(request, f"project_page/{project_id}.html", context)
-
