7669192c676cc1a1ec3cd2b38fa65d91fe22e7de
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-02 19:22:20 -0500

    updated return for project page view

--- portfolio_website/project_page/views.py
+++ portfolio_website/project_page/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request, project_id):
     s = f"<h1>Project Name:{project_id}<h1>"
-    return HttpResponse(s)
-    # return render(request, "project_page/base_project_page.html")
+    # return HttpResponse(s)
+    return render(request, "project_page/base_project_page.html")
 
