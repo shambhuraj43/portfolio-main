428296d49fffb80a3328d2c66311c4f90d568f1a
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-02 19:09:25 -0500

    paratmeterized view for project_page

--- portfolio_website/project_page/views.py
+++ portfolio_website/project_page/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request, "project_page/base_project_page.html")
+def index(request, project_id):
+    s = f"<h1>Project Name:{project_id}<h1>"
+    return HttpResponse(s)
+    # return render(request, "project_page/base_project_page.html")
 
