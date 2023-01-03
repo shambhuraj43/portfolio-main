3e3376dcc3b1158c61912f509067fee4cc3816e5
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-03 05:56:29 -0500

    modularized `Independent Projects` section in base.html

--- portfolio_website/resume_section/views.py
+++ portfolio_website/resume_section/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def resume_page(request):
-    return render(request, "resume_section/base.html")
+    context = {
+        "projects": [
+            {
+                "name": "base",
+                "technologies": "STRING OF TECHNOLOGIES"
+            },
+            {
+                "name": "PROJECT1",
+                "technologies": "STRING OF TECHNOLOGIES"
+            },
+            {
+                "name": "PROJECT1",
+                "technologies": "STRING OF TECHNOLOGIES"
+            },
+            {
+                "name": "PROJECT1",
+                "technologies": "STRING OF TECHNOLOGIES"
+            }
+        ]
+    }
+    return render(request, "resume_section/base.html", context)
