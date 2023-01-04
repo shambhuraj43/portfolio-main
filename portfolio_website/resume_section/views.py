1292d13787ea48054bba2228b6d96b359b0a8fea
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-03 22:50:27 -0500

    created views for redirection

--- portfolio_website/resume_section/views.py
+++ portfolio_website/resume_section/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -25,3 +25,27 @@ def resume_page(request):
         ]
     }
     return render(request, "resume_section/base.html", context)
+
+def education(request):
+    response = redirect('http://127.0.0.1:8000/resume/#education')
+    return response
+
+def professional_experience(request):
+    response = redirect('http://127.0.0.1:8000/resume/#professional_experience')
+    return response
+
+def publications(request):
+    response = redirect('http://127.0.0.1:8000/resume/#publications')
+    return response
+
+def leadership(request):
+    response = redirect('http://127.0.0.1:8000/resume/#leadership')
+    return response
+
+def skills(request):
+    response = redirect('http://127.0.0.1:8000/resume/#skills')
+    return response
+
+def projects(request):
+    response = redirect('http://127.0.0.1:8000/resume/#projects')
+    return response
