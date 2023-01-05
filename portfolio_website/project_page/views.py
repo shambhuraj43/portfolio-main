1d500e28fa4c54313b32aa1603d25d9566bfef1e
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-04 21:44:41 -0500

    modified project page context as multiple objects

--- portfolio_website/project_page/views.py
+++ portfolio_website/project_page/views.py
@@ -4,41 +4,81 @@ from django.shortcuts import render
 def index(request, project_id):
 
     context = {
-        "project_name": "PROJECT_NAME",
-        "project_description": "PROJECT_DESCRIPTION",
-        "technolgies_used": [
-            {
-                "name": "docker",
-                "description": "TECHNOLOGY DESCRIPTION",
-                "icon":'docker'
-            },
-            {
-                "name": "docker",
-                "description": "TECHNOLOGY DESCRIPTION",
-                "icon":"none"
-            },
-            {
-                "name": "docker",
-                "description": "TECHNOLOGY DESCRIPTION",
-                "icon":"none"
-            },
-        ],
-        "installation_requirements": [
-            {
-                "req":"PYTHON",
-                "version":"VERSION"
-            },
-            {
-                "req":"PYTHON",
-                "version":"VERSION"
-            }, 
-            {
-                "req":"PYTHON",
-                "version":"VERSION"
-            }, 
-        ],
-        "requirements_file":"Requirements.txt",
-        "requirements_file_link":"https://github.com/shambhuraj43/portfolio-main",
-        "github_repo_link": "https://github.com/shambhuraj43/portfolio-main"
+        "portfolio": {
+            "project_name": "PROJECT_NAME",
+            "project_description": "PROJECT_DESCRIPTION",
+            "technolgies_used": [
+                {
+                    "name": "docker",
+                    "description": "TECHNOLOGY DESCRIPTION",
+                    "icon": 'docker'
+                },
+                {
+                    "name": "docker",
+                    "description": "TECHNOLOGY DESCRIPTION",
+                    "icon": "none"
+                },
+                {
+                    "name": "docker",
+                    "description": "TECHNOLOGY DESCRIPTION",
+                    "icon": "none"
+                },
+            ],
+            "installation_requirements": [
+                {
+                    "req": "PYTHON",
+                           "version": "VERSION"
+                },
+                {
+                    "req": "PYTHON",
+                           "version": "VERSION"
+                },
+                {
+                    "req": "PYTHON",
+                           "version": "VERSION"
+                },
+            ],
+            "requirements_file": "Requirements.txt",
+            "requirements_file_link": "https://github.com/shambhuraj43/portfolio-main",
+            "github_repo_link": "https://github.com/shambhuraj43/portfolio-main"
+        },
+        "django-rest-api": {
+            "project_name": "PROJECT_NAME",
+            "project_description": "PROJECT_DESCRIPTION",
+            "technolgies_used": [
+                {
+                    "name": "docker",
+                    "description": "TECHNOLOGY DESCRIPTION",
+                    "icon": 'docker'
+                },
+                {
+                    "name": "docker",
+                    "description": "TECHNOLOGY DESCRIPTION",
+                    "icon": "none"
+                },
+                {
+                    "name": "docker",
+                    "description": "TECHNOLOGY DESCRIPTION",
+                    "icon": "none"
+                },
+            ],
+            "installation_requirements": [
+                {
+                    "req": "PYTHON",
+                           "version": "VERSION"
+                },
+                {
+                    "req": "PYTHON",
+                           "version": "VERSION"
+                },
+                {
+                    "req": "PYTHON",
+                           "version": "VERSION"
+                },
+            ],
+            "requirements_file": "Requirements.txt",
+            "requirements_file_link": "https://github.com/shambhuraj43/portfolio-main",
+            "github_repo_link": "https://github.com/shambhuraj43/portfolio-main"
+        }
     }
-    return render(request, f"project_page/{project_id}.html", context)
+    return render(request, f"project_page/{project_id}.html", context[project_id])
