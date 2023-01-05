546c5a1b4a339e9c89fc5a1ef4e4629bb07998c3
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-04 23:26:32 -0500

    removed unnecessary url paths

--- portfolio_website/landing_page/urls.py
+++ portfolio_website/landing_page/urls.py
@@ -4,7 +4,4 @@ from landing_page import views
 app_name = 'landing_page'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('generic/', views.generic, name='generic'),
-    path('generic/', views.generic, name='generic'),
-    path('elements/', views.elements, name='elements'),
 ]
