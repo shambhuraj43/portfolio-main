8621ab9d755d135d4e23b6579aab20747ecc66ee
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-06 14:24:08 -0500

    created `config.py` to store contexts

---
+++ portfolio_website/config.py
@@ -0,0 +1,36 @@
+projects_context = {
+        "projects": [
+            {
+                "name": "Portfolio Website",
+                "technologies": "Python, Django, Docker, GraphQL, GitHub Actions, AWS (Elastic Bean), Apache2, TDD, MySQL"
+            },
+            {
+                "name": "REST API Project",
+                "technologies": "Python, Django, Django - REST Framework, REST API, SQLite"
+            },
+            {
+                "name": "Habit Tracker",
+                "technologies": "MERN Stack, REST API, MongoDB NoSQL, Heroku, GitHub Pages, Docker"
+            },
+            {
+                "name": "Software Design Calculator",
+                "technologies":"Software Design, SOLID Principles"
+            },
+            {
+                "name": "Random Contact Card",
+                "technologies": "Node JS, Express, MongoDB, Third Party API Integration, Heroku"
+            },
+            {
+                "name": "Weather Application",
+                "technologies": "Node JS, Express, Third Party API Integration, Heroku"
+            },
+            {
+                "name": "Path Finder",
+                "technologies": "Java, Graph Theory"
+            },
+            {
+                "name": "Banking Application",
+                "technologies": "Java, OOP Concepts"
+            }
+        ]
+    }
