8f1e2ec43ff261857fb5978a013c9244ffee1f9a
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-07 14:30:41 -0500

    imported renamed context from `config.py`

--- portfolio_website/project_page/views.py
+++ portfolio_website/project_page/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from config import project_page_context
+from config import projects_information
 
 
 def base_page(request, project_id):
-    return render(request, f"project_page/base.html", project_page_context[project_id])
+    return render(request, f"project_page/base.html", {"project":projects_information[project_id]})
 
 def github_page(request, project_id):
     response = redirect(f"https://github.com/shambhuraj43/{project_id}")
