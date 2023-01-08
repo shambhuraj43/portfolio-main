62f7e8290eef79ba968280d400e729faa883079b
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-07 22:38:50 -0500

    added extra necessary urls

--- portfolio_website/resume_page/urls.py
+++ portfolio_website/resume_page/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from resume_page import views
 
+app_name = 'resume_page'
+
 urlpatterns = [
-    path('new_resume', views.index, name="index")
+    path('', views.resume_page, name="resume_page"),
+    path('education', views.education, name='education'),
+    path('professional_experience', views.professional_experience, name='professional_experience'),
+    path('publications', views.publications, name='publications'),
+    path('leadership', views.leadership, name='leadership'),
+    path('skills', views.skills, name='skills'),
+    path('projects', views.projects, name='projects'),
 ]
