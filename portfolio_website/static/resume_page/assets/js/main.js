662c14faedc5a3118f9ba3e217c28dd3b7cebbfa
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-07 20:55:09 -0500

    js changes

--- portfolio_website/static/resume_page/assets/js/main.js
+++ portfolio_website/static/resume_page/assets/js/main.js
@@ -4,8 +4,8 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
 
+(function($) {
 	var	$window = $(window),
 		$body = $('body'),
 		$main = $('#main');
