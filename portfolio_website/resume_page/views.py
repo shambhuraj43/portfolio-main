3d1e189f0f38399e4eb423279257af5bc7ec38c1
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-07 21:19:37 -0500

    added context to index

--- portfolio_website/resume_page/views.py
+++ portfolio_website/resume_page/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from config import projects_context
 
 # Create your views here.
 def index(request):
-    return render(request, "resume_page/base.html", {})
+    return render(request, "resume_page/base.html", {"projects":projects_context})
